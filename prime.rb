9da4b4352d511417e9a0ae2f00f5ad4c614e5778
Alma Flores <flores.almalee@gmail.com>
2020-08-19 02:51:48 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
 # Add  code here!
+def prime?(num)
+  prime = true
+  if num < 2 
+    prime = false 
+  else 
+    n = 2 
+    while n < num
+      prime = false if num % n == 0 
+      n += 1 
+    end
+  end
+  prime
+end
