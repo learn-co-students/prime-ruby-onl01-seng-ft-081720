d77bdca7b7b5f2759da41c373938915d182df587
Tabatha Slatton <valbatha@gmail.com>
2020-08-19 15:59:23 -0600

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
 # Add  code here!
+def prime?(num)
+    factors = []
+    i = 1
+    if num <= 1
+        return false
+    end
+    while i < num
+     if num % i === 0 
+        factors.push(i)
+        if i * i === num
+            factors.push(i)
+        end
+     end
+        i += 1
+    end
+    factors.length > 2 ? false : true
+end
