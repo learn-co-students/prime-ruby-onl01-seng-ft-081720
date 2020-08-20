9b39bbdf485a7bc1c4473eee2c67c3d5df6721f0
muazzamnashat <muazzamhc.nashat@gmail.com>
2020-08-20 05:37:45 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+require 'benchmark'
+
+def prime? (num)
+    if num == 2 
+      return true 
+    end 
+    if num <= 0 || num == 1
+      return false
+    end
+    (2..num-1).none? do |i|
+        num % i == 0
+    end
+
+end
+
+puts Benchmark.measure {prime? 100000}
