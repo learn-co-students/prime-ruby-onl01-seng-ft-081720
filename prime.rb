f1d90a24db5add984c0ba8ec6f9c6a5f1787f1f5
Adam Beatty <adam.t.beatt@gmail.com>
2020-08-20 18:03:35 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
 # Add  code here!
+def prime?(int)
+  if int < 2
+    return false
+  end
+
+  counter = 2
+  while counter <= int / 2
+    if int % counter == 0
+      return false
+    end
+    counter += 1
+
+  end
+
+  return true
+end
