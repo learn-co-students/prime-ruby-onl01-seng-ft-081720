589cee2b7304747f2a8018674e9407f78d5d1673
JDC1492 <Jonathan_Cohen10@yahoo.com>
2020-08-23 19:42:26 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,9 +1,6 @@
 def prime?(num)
-    n = 2
-    while n < num || num <= 1
-      return false if num % n == 0 
-      n += 1
-    end
-    true
+  return false if num < 2
+  return true if num == 2
+  (2..num - 1).each {|int| return false if num % int ==0}
+true
 end
-
