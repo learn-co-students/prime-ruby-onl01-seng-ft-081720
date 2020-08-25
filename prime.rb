ecfc5f44dd06451a4f0e9b3366b12e0829d13389
SophiaGrace16 <sophia.g.white@outlook.com>
2020-08-25 14:05:32 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,9 @@
-# Add  code here!
+def prime?(number)    
+    integer = 2   
+    if number > 1        
+    array = (integer..number - 1).to_a         
+    array.none? {|integer|number% integer == 0 }    
+    else    
+        false    
+    end  
+end
